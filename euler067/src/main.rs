f46f902aef33e07a0dd20bb14c3171b69fd69dd2
Connor Hilarides <connorcpu@live.com>
2017-03-01 21:36:04 -0800

    067

---
+++ euler067/src/main.rs
@@ -0,0 +1,28 @@
+use std::mem::swap;
+
+static TRIANGLE: &[&[i32]] = include!("p067_triangle.rs");
+
+fn main() {
+    let mut row = TRIANGLE.len() - 1;
+    let mut values = TRIANGLE[row].to_vec();
+    let mut next = Vec::with_capacity(TRIANGLE[row].len() - 1);
+
+    while row > 0 {
+        row -= 1;
+
+        next.extend_from_slice(TRIANGLE[row]);
+
+        for (i, parent) in next.iter_mut().enumerate() {
+            if values[i] >= values[i + 1] {
+                *parent += values[i];
+            } else {
+                *parent += values[i + 1];
+            }
+        }
+
+        swap(&mut values, &mut next);
+        next.clear();
+    }
+
+    println!("{}", values[0]);
+}
