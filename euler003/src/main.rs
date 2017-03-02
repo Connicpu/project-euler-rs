49b5e0b0325f7815630a9f1fa39fb46452753495
Connor Hilarides <connorcpu@live.com>
2017-03-02 00:36:47 -0800

    Simplify 003 with `primal`

--- euler003/src/main.rs
+++ euler003/src/main.rs
@@ -1,25 +1,22 @@
+extern crate primal;
+
+use primal::Primes;
+
 // sqrt(600851475143) = 775146.099
 const MAX_NUM: usize = 775147;
 const NUMERATOR: usize = 600851475143;
 
 fn main() {
-    let mut prime_states = [true; MAX_NUM];
-    let mut largest_factor = 1;
+    let mut largest_factor = 0;
 
     // Fill it out like a sieve of eratosthenese
-    for i in 2..MAX_NUM {
-        if !prime_states[i] {
-            continue;
+    for prime in Primes::all() {
+        if prime > MAX_NUM {
+            break;
         }
 
-        let mut j = i * 2;
-        while j < MAX_NUM {
-            prime_states[j] = false;
-            j += i;
-        }
-
-        if NUMERATOR % i == 0 {
-            largest_factor = i;
+        if NUMERATOR % prime == 0 {
+            largest_factor = prime;
         }
     }
 
