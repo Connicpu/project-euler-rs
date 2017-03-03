d8af64b2dc3f4597b3e7380b8235e6b07dc9a834
Connor Hilarides <connorcpu@live.com>
2017-03-03 12:42:35 -0800

    015

---
+++ euler015/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(i128_type, inclusive_range_syntax)]
+
+fn fac_ratio(n: i128, k: i128) -> i128 {
+    (k + 1..n + 1).fold(1, |i, a| i * a)
+}
+
+fn fac(n: i128) -> i128 {
+    fac_ratio(n, 1)
+}
+
+fn main() {
+    let n = 20;
+
+    println!("{}", fac_ratio(n + n, n) / fac(n));
+}
