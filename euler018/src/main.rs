15cd276325c2ff2017a8bece28589d57221647be
Connor Hilarides <connorcpu@live.com>
2017-03-03 12:56:29 -0800

    018

---
+++ euler018/src/main.rs
@@ -0,0 +1,20 @@
+use std::cmp::max;
+
+static TRIANGLE: &[&[i32]] = include!("p018_triangle.rs");
+
+fn main() {
+    let mut row = TRIANGLE.len() - 1;
+    let mut values = TRIANGLE[row].to_vec();
+
+    while row > 0 {
+        row -= 1;
+
+        let row = TRIANGLE[row];
+        for i in 0..row.len() {
+            let bigger = max(values[i], values[i + 1]);
+            values[i] = row[i] + bigger;
+        }
+    }
+
+    println!("{}", values[0]);
+}
